06465b04c66b9dbab5c4c6b6b87725879c6b8adc
Nowicki123 <onowicki2018@gmail.com>
2023-10-16 09:35:09 +0200

    16_10_2023

---
+++ project_16_10_2023/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+
+Route::prefix('admin')->group(function(){
+    Route::get('/users', function(){
+        return '<h3> Użytkownicy </h3>';
+    });
+    
+    Route::get('/home', function(){
+        return '<h3> Strona Domowa </h3>';
+    });
+    
+});
+
+
+Route::redirect('/admin', '/admin/home');
